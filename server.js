f550c1fdd36a7103ad3db97b648e8b12b3df8cd3
Toka_Kun_ <222786419+toka-kun@users.noreply.github.com>
2026-05-19 10:08:14 +0900

    Update audio stream filtering for 1080p MP4
    Filter audio streams to only include 1080p MP4 format.

--- server.js
+++ server.js
@@ -92,13 +92,9 @@ app.get('/api/video/:id', async (req, res) => {
     const formatStreams = videoInfo.formatStreams || [];
     const streamUrl = formatStreams.reverse().map(stream => stream.url)[0];
     const audioStreams = videoInfo.adaptiveFormats || [];
-    
-    // 修正箇所: mp4の中で解像度を持つものをフィルタリングし、解像度の数値で降順ソートして一番高いものを取得
     let highstreamUrl = audioStreams
-      .filter(stream => stream.container === 'mp4' && stream.resolution)
-      .sort((a, b) => parseInt(b.resolution) - parseInt(a.resolution))
+      .filter(stream => stream.container === 'mp4' && stream.resolution === '1080p')
       .map(stream => stream.url)[0];
-
     const audioUrl = audioStreams
       .filter(stream => stream.container === 'm4a' && stream.audioQuality === 'AUDIO_QUALITY_MEDIUM')
       .map(stream => stream.url)[0];
